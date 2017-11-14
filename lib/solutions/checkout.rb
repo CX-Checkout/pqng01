26d865933d4df061d705000309e1375744394969
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-11-14 16:25:26 +0000

    Tue Nov 14 16:25:26 GMT 2017

--- lib/solutions/checkout.rb
+++ lib/solutions/checkout.rb
@@ -1,8 +1,51 @@
 # noinspection RubyUnusedLocalVariable
 class Checkout
 
+  def initialize
+    @prices = {
+        :A => 50,
+        :B => 30,
+        :C => 20,
+        :D => 15
+    }
+
+    @special_offer_prices = {
+        :A => 130,
+        :B => 45
+    }
+
+  end
+
   def checkout(skus)
-    raise 'Not implemented'
+    return 0 if skus == "";
+
+    return -1 if skus.match(/\W+/)
+    return -1 if skus.match(/[a-z]+/)
+
+    skus = skus.chars.sort.join
+
+    special_offers = ""
+    skus.scan(/AAA/) do |special_offer_A|
+      special_offers += "A"
+    end
+    skus.gsub! 'AAA', ''
+
+    skus.scan(/BB/) do |special_offer_B|
+      special_offers += "B"
+    end
+    skus.gsub! 'BB', ''
+
+    total = 0
+
+    skus.each_char do |product|
+      total += @prices[product.to_sym]
+    end
+
+    special_offers.each_char do |special_offer|
+      total += @special_offer_prices[special_offer.to_sym]
+    end
+
+    return total
   end
 
 end
---
+++ test/checkout_test.rb
@@ -0,0 +1,52 @@
+# noinspection RubyResolve,RubyResolve
+require_relative 'test_helper'
+require 'logging'
+
+Logging.logger.root.appenders = Logging.appenders.stdout
+
+class ClientTest < Minitest::Test
+    def test_checkout_should_return_0_for_entry_SKU
+      assert_equal 0, Checkout.new.checkout("")
+    end
+
+    def test_checkout_should_return_50_for_A
+      assert_equal 50, Checkout.new.checkout("A")
+    end
+
+    def test_checkout_should_return_30_for_B
+      assert_equal 30, Checkout.new.checkout("B")
+    end
+
+    def test_checkout_should_return_20_for_C
+      assert_equal 20, Checkout.new.checkout("C")
+    end
+
+    def test_checkout_should_return_15_for_D
+      assert_equal 15, Checkout.new.checkout("D")
+    end
+
+    def test_checkout_should_handle_multiple_products
+      assert_equal 100, Checkout.new.checkout("AA")
+    end
+
+    def test_checkout_should_give_discount_for_multiple_A
+      assert_equal 130, Checkout.new.checkout("AAA")
+    end
+
+    def test_checkout_should_give_discount_for_multiple_B
+      assert_equal 45, Checkout.new.checkout("BB")
+    end
+
+    def test_checkout_should_give_discount_for_out_of_order_multiple_products
+      assert_equal 175, Checkout.new.checkout("ABABA")
+    end
+
+    def test_checkout_should_handle_multiple_products_in_random_order
+      assert_equal 380, Checkout.new.checkout("AABCCBCBBDBACCB")
+    end
+
+    def test_checkout_should_handle_unknown_items
+      assert_equal -1, Checkout.new.checkout("x")
+    end
+
+end
